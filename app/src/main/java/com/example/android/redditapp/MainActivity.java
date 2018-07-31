535060a7948bc704113b8454e230c9472b936fe2
Hugo Valente <hugomika68@gmail.com>
2018-07-31 13:37:43 +0100

    Added provider and handled empty cursor

--- app/src/main/java/com/example/android/redditapp/MainActivity.java
+++ app/src/main/java/com/example/android/redditapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.redditapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
@@ -11,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.Call;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,6 +26,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.example.android.redditapp.Connection.ConnectionManager;
 import com.example.android.redditapp.Connection.Request;
+import com.example.android.redditapp.DB.DatabaseContract;
 import com.example.android.redditapp.models.Post.Child;
 import com.example.android.redditapp.models.Post.Post;
 import com.google.gson.Gson;
@@ -131,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
                     Picasso.get().load(imageURL).into(thumbnail);
                 }
 
+                addMovieToDb(posts);
 
 
             }
@@ -147,4 +152,62 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private void addMovieToDb(List<Post> postList) {
+
+        Cursor mCursor = getAllPosts();
+
+        for (Post post : postList) {
+
+            boolean isInDB = false;
+
+            // If the cursor is empty add all posts
+            if(((mCursor != null) && (mCursor.getCount() > 0))) {
+
+
+                mCursor.moveToFirst();
+
+                Log.d("First1", "Post Id: " + mCursor.getColumnIndex(DatabaseContract.PostsTable.POSTID));
+
+                Log.d("First2", "Post Id: " + postList.get(0).getData().getChildren().get(0).getData().getId());
+
+                while (mCursor.moveToNext()) {
+                    Log.d("Main1", "Post id: " + mCursor.getColumnIndex(DatabaseContract.PostsTable.POSTID));
+                    Log.d("Main2", "Post position: " + post.getData().getChildren().get(0).getData().getId());
+
+                    if (post.getData().getChildren().get(0).getData().getId().equals((mCursor.getString(mCursor.getColumnIndex(DatabaseContract.PostsTable.POSTID))))) {
+                        isInDB = true;
+                    }
+                }
+
+            }
+
+            if (!isInDB) {
+
+                ContentValues cv = new ContentValues();
+
+                cv.put(DatabaseContract.PostsTable.SUBREDDIT, post.getData().getChildren().get(0).getData().getSubreddit());
+                cv.put(DatabaseContract.PostsTable.POSTID, post.getData().getChildren().get(0).getData().getId());
+                cv.put(DatabaseContract.PostsTable.TITLE, post.getData().getChildren().get(0).getData().getTitle());
+                cv.put(DatabaseContract.PostsTable.AUTHOR, post.getData().getChildren().get(0).getData().getAuthor());
+                cv.put(DatabaseContract.PostsTable.SELFTEXT, post.getData().getChildren().get(0).getData().getSelftext());
+                cv.put(DatabaseContract.PostsTable.IMAGELINK, post.getData().getChildren().get(0).getData().getThumbnail());
+
+                Uri uri = getContentResolver().insert(DatabaseContract.CONTENT_URI, cv);
+                Log.d("Uri", "Uri: " + uri);
+            }
+        }
+
+
+    }
+
+    private Cursor getAllPosts() {
+        // fazer query de todos os filmes de uma categoria
+        Cursor mCursor;
+        mCursor = getContentResolver().query(DatabaseContract.CONTENT_URI, null, null, null, null);
+
+
+        return mCursor;
+    }
+
 }
